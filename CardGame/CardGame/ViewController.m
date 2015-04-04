2845bb4dc6dd718f0fe2f0fbf87e9be62f1b81a2
PJDesi <pdesamer@slu.edu>
2015-04-04 16:45:50 -0500

    Add game logic. But still need help with int and NSUInteger conversion

--- CardGame/CardGame/ViewController.m
+++ CardGame/CardGame/ViewController.m
@@ -7,38 +7,80 @@
 //
 
 #import "ViewController.h"
+#import "Deck.h"
+#import "PlayingCardDeck.h"
+#import "CardMatchingGame.h"
 
 @interface ViewController ()
-
+@property (strong, nonatomic) CardMatchingGame *game;
+@property (strong, nonatomic) Deck *deck;
+@property (nonatomic) int flipCount;
+@property (strong, nonatomic) IBOutletCollection(UIButton) NSArray *cardButtons;
+@property (weak, nonatomic) IBOutlet UILabel *scoreLabel;
 @end
 
 @implementation ViewController
 
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+- (CardMatchingGame *)game
+{
+    if (!_game) _game = [[CardMatchingGame alloc] initWithCardCount:[self.cardButtons count] usingDeck:[self createDeck]];
+    
+    return _game;
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (Deck *)createDeck
+{
+    return [[PlayingCardDeck alloc] init];
 }
-- (IBAction)touchCardButton:(UIButton *)sender {
+
+
+- (IBAction)touchCardButton:(UIButton *)sender
+{
     
-    if ([sender.currentTitle length]) {
-        //local variable to hold the cardBack image
-        UIImage *cardImage = [UIImage imageNamed:@"cardBack"];
-        [sender setBackgroundImage:cardImage forState:UIControlStateNormal];
-        [sender setTitle:@"" forState:UIControlStateNormal];
-    } else {
-        //local variable to hold the cardBack image
-        UIImage *cardImage = [UIImage imageNamed:@"cardFront"];
-        [sender setBackgroundImage:cardImage forState:UIControlStateNormal];
-        
-        [sender setTitle:@"A♣️" forState:UIControlStateNormal];
+//    if ([sender.currentTitle length]) {
+//        //local variable to hold the cardBack image
+//        UIImage *cardImage = [UIImage imageNamed:@"cardBack"];
+//        [sender setBackgroundImage:cardImage forState:UIControlStateNormal];
+//        [sender setTitle:@"" forState:UIControlStateNormal];
+//    } else {
+//        Card *randomCard = [self.deck drawRandomCard];
+//        if (randomCard) { //Protect against an empty deck
+//            //local variable to hold the cardBack image
+//            UIImage *cardImage = [UIImage imageNamed:@"cardFront"];
+//            [sender setBackgroundImage:cardImage forState:UIControlStateNormal];
+//            [sender setTitle:randomCard.contents forState:UIControlStateNormal];
+//        }
+//    }
+//    self.flipCount++;
+//    NSLog(@"Touch Count is: %d", self.flipCount);
+    NSLog(@"In Touch Card Button");
+    NSInteger chosenButtonIndex = [self.cardButtons indexOfObject:sender];
+    [self.game chooseCardAtIndex:chosenButtonIndex];
+    [self updateUI];
+}
+
+- (void)updateUI
+{
+    NSLog(@"In UpdateUI");
+    for (UIButton *cardButton in self.cardButtons) {
+        NSInteger cardButtonIndex = [self.cardButtons indexOfObject:cardButton];
+        Card *card = [self.game cardAtIndex:cardButtonIndex];
+        NSLog(@"In UpdateUI For loop");
+        [cardButton setTitle:[self titleForCard:card] forState:UIControlStateNormal];
+        [cardButton setBackgroundImage:[self backgroungImageForCard:card] forState:UIControlStateNormal];
+        cardButton.enabled = !card.isMatched;
     }
-   
     
+}
+
+- (NSString *)titleForCard:(Card *)card
+{
+    return card.isChosen ? card.contents : @"";
+}
+
+- (UIImage *)backgroungImageForCard:(Card *)card
+{
+    return [UIImage imageNamed:card.isChosen ? @"cardFront" : @"cardBack"];
 }
 
 @end
